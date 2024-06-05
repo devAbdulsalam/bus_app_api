109c54be5fc6021e2f2cdb51132efbaabe52c271
devabdulsalam <ammuftau74@gmail.com>
2024-06-05 09:39:23 +0100

    add ssl

--- db.js
+++ db.js
@@ -5,8 +5,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
+const pool = new Pool({ user: 'bus_app_t3i9_user',
+  host: 'dpg-cpfkkfgl5elc738934k0-a.oregon-postgres.render.com',
+  database: 'bus_app_t3i9',
+  password: 'J4KjKNLXMDX9nJtSAWhBH39Ej6J7Ceah',
+  port: 5432,
+	// connectionString: process.env.DATABASE_URL,
 	ssl: {
     rejectUnauthorized: false, // Use this option only if you do not have a valid certificate
   },
