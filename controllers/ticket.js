f12e4623bd4aad5263bbe1aaf84090b26e0027fe
devabdulsalam <ammuftau74@gmail.com>
2024-06-07 12:35:26 +0100

    fix get bus

--- controllers/ticket.js
+++ controllers/ticket.js
@@ -55,6 +55,15 @@ export const bookTicket = async (req, res) => {
 				.status(400)
 				.json({ message: 'Bus is filled up, book another bus' });
 		}
+
+		// check if seat is not taken
+		const seat_is_available = await pool.query(
+			'SELECT * FROM tickets WHERE id = $1 AND seat_number = $2',
+			[bus_id, seat_number]
+		);
+		if (seat_is_available.rows[0]) {
+			return res.status(400).json({ message: 'Seat booked by another user!' });
+		}
 		const ticketResult = await pool.query(
 			`INSERT INTO tickets (user_id, bus_id, seat_number, booking_date, status, created_at)
        VALUES ($1, $2, $3, NOW(), 'BOOKED', NOW()) RETURNING *`,
@@ -68,6 +77,52 @@ export const bookTicket = async (req, res) => {
 
 		await pool.query('COMMIT');
 		res.status(201).json(ticketResult.rows[0]);
+	} catch (error) {
+		await pool.query('ROLLBACK');
+		res.status(500).json({ error: error.message });
+	}
+};
+export const bookTickets = async (req, res) => {
+	const { bus_id, seat_numbers } = req.body;
+	const user_id = req.user.id;
+
+	try {
+		await pool.query('BEGIN');
+
+		const busResult = await pool.query('SELECT * FROM bus WHERE id = $1', [
+			bus_id,
+		]);
+		const bus = busResult.rows[0];
+		if (!bus) {
+			return res.status(400).json({ message: 'Bus not found!' });
+		}
+		if (bus.avalable_seats < seat_numbers.length) {
+			return res.status(400).json({ message: 'Insufficient available seats!' });
+		}
+
+		for (const seatNumber of seat_numbers) {
+			const seat_is_available = await pool.query(
+				'SELECT * FROM tickets WHERE id = $1 AND seat_number = $2',
+				[bus_id, seatNumber]
+			);
+			if (seat_is_available.rows[0]) {
+				return res.status(400).json({ message: `Seat ${seatNumber} booked!` });
+			}
+
+			await pool.query(
+				`INSERT INTO tickets (user_id, bus_id, seat_number, booking_date, status, created_at)
+         VALUES ($1, $2, $3, NOW(), 'BOOKED', NOW())`,
+				[user_id, bus_id, seatNumber]
+			);
+		}
+
+		await pool.query(
+			`UPDATE bus SET available_seats = available_seats - ${seat_numbers.length} WHERE id = $1`,
+			[bus_id]
+		);
+
+		await pool.query('COMMIT');
+		res.status(201).json({ message: 'Seats booked successfully!' });
 	} catch (error) {
 		await pool.query('ROLLBACK');
 		res.status(500).json({ error: error.message });
