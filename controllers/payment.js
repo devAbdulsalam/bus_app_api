f2d1a39e3a39c5b23538b21555526593bd3783ed
devabdulsalam <ammuftau74@gmail.com>
2024-06-10 16:16:29 +0100

    fix monnify transactions

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,6 +1,6 @@
 import getToken from '../utils/monnify.js';
 
-export const create = async (req, res) => {
+export const createPayment = async (req, res) => {
 	const { customerName, customerEmail, amount } = req.body;
 
 	const accessToken = await getToken();
@@ -32,6 +32,77 @@ export const create = async (req, res) => {
 		res.json({ paymentUrl: data?.responseBody?.checkoutUrl });
 	} catch (error) {
 		console.log(error);
-		res.status(500).json({ error: error?.message });
+		res
+			.status(500)
+			.json({ error: error?.message || error?.responseMessage || error });
+	}
+};
+
+export const confrimTransaction = async (req, res) => {
+	const { paymentReference } = req.body;
+	try {
+		const accessToken = await getToken();
+
+		// paymentReference = reference12345;
+
+		const response = await fetch(
+			`https://sandbox.monnify.com/api/v1/merchant/transactions/query?paymentReference=${paymentReference}`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			}
+		);
+
+		const data = await response.json();
+		res.json(data);
+	} catch (error) {
+		console.error('Error confirming transaction:', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+};
+export const getPayment = async (req, res) => {
+	const { id } = req.params;
+	try {
+		const accessToken = await getToken();
+		// MNFY | 20190809123429 | 000000;
+
+		const response = await fetch(
+			`https://sandbox.monnify.com/api/v1/merchant/transactions/query??transactionReference=${id}`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			}
+		);
+
+		const data = await response.json();
+		res.json(data);
+	} catch (error) {
+		console.error('Error confirming transaction:', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+};
+export const getPayments = async (req, res) => {
+	try {
+		const accessToken = await getToken();
+
+		const response = await fetch(
+			`https://https://sandbox.monnify.com/api/v1/transactions/search`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			}
+		);
+
+		const data = await response.json();
+		res.json(data);
+	} catch (error) {
+		console.error('Error getting transaction:', error);
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 };
--- routes/payment.js
+++ routes/payment.js
@@ -1,6 +1,14 @@
 import express from 'express';
-import { create } from '../controllers/payment.js';
-// import auth from '../middlewares/auth.js';
+import {
+	confrimTransaction,
+	createPayment,
+	getPayments,
+	getPayment,
+} from '../controllers/payment.js';
+import auth from '../middlewares/auth.js';
 const router = express.Router();
-router.post('/', create);
+router.get('/', auth, getPayments);
+router.get('/:id', auth, getPayment);
+router.post('/', auth, createPayment);
+router.post('/', auth, confrimTransaction);
 export default router;
