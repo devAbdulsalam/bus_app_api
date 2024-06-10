553432f2666b4b90c3e004d75dc4ca19813ab0dc
devabdulsalam <ammuftau74@gmail.com>
2024-06-10 15:10:45 +0100

    add monnify test

--- controllers/payment.js
+++ controllers/payment.js
@@ -4,29 +4,34 @@ export const create = async (req, res) => {
 	const { customerName, customerEmail, amount } = req.body;
 
 	const accessToken = await getToken();
-	console.log('accessToken', accessToken);
+	// console.log('accessToken', accessToken);
+	try {
+		const response = await fetch(
+			'https://sandbox.monnify.com/api/v1/merchant/transactions/init-transaction',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${accessToken}`,
+				},
+				body: JSON.stringify({
+					amount,
+					customerName,
+					customerEmail,
+					paymentReference: `ref_${Date.now()}`,
+					paymentDescription: 'Payment for goods',
+					currencyCode: 'NGN',
+					contractCode: 8794558373,
+					redirectUrl: '/',
+				}),
+			}
+		);
 
-	const response = await fetch(
-		'https://sandbox.monnify.com/api/v1/merchant/transactions/init-transaction',
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${accessToken}`,
-			},
-			body: JSON.stringify({
-				amount,
-				customerName,
-				customerEmail,
-				paymentReference: `ref_${Date.now()}`,
-				paymentDescription: 'Payment for goods',
-				currencyCode: 'NGN',
-				contractCode: 8794558373,
-				redirectUrl: '/',
-			}),
-		}
-	);
-
-	const data = await response.json();
-	res.json({ paymentUrl: data.responseBody.checkoutUrl });
+		const data = await response.json();
+		console.log(data);
+		res.json({ paymentUrl: data?.responseBody?.checkoutUrl });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: error?.message });
+	}
 };
